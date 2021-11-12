8369be5244e589ed27cadd80a147542d6aaf6bb5
KHS-PC\pskna <psknal27@gmail.com>
2021-11-12 16:19:12 +0900

    fix. 몇 일 전부터 알람 울리는 로직 수정

--- Alamemo/app/src/main/java/com/landvibe/alamemonew/util/AlarmHandler.kt
+++ Alamemo/app/src/main/java/com/landvibe/alamemonew/util/AlarmHandler.kt
@@ -43,13 +43,9 @@ class AlarmHandler {
         val todayCalendar = Calendar.getInstance()
         val alarmCalendar = Calendar.getInstance().apply {
             memo.scheduleDateYear.value?.let { year -> set(Calendar.YEAR, year) }
-            memo.scheduleDateMonth.value?.let { month -> set(Calendar.YEAR, month) }
-            memo.scheduleDateDay.value?.let { day -> set(Calendar.YEAR, day) }
-            memo.alarmStartTimeHour.value?.let { hour -> set(Calendar.YEAR, hour) }
-            memo.alarmStartTimeMinute.value?.let { minute -> set(Calendar.YEAR, minute) }
+            memo.scheduleDateMonth.value?.let { month -> set(Calendar.MONTH, month) }
+            memo.scheduleDateDay.value?.let { day -> set(Calendar.DAY_OF_MONTH, day) }
         }
-
-        Log.d("firstTime::", alarmCalendar.time.toString())
 
         if(memo.alarmStartTimeType.value == 1) {
             val alarmHour = memo.alarmStartTimeHour.value
@@ -68,7 +64,7 @@ class AlarmHandler {
             // 아니라면 알람이 시작하기로 설정된 날에 맞춰서 알람을 설정하면 된다.
             memo.alarmStartTimeType.value?.let { alarmStartTimeType ->
                 //타입에 따라서 일 수를 빼준다.
-                alarmCalendar.time.time -= getDayByAlarmStartType(alarmStartTimeType)
+                alarmCalendar.add(Calendar.DAY_OF_MONTH, getDayByAlarmStartType(alarmStartTimeType))
             }
         }
 
@@ -85,16 +81,16 @@ class AlarmHandler {
     }
 
     //알람 시작일에 따른 값을 return. 1 : 매일, 2 : 1주일 전, 3 : 3일 전, 4 : 하루 전
-    private fun getDayByAlarmStartType(type: Int) : Long {
+    private fun getDayByAlarmStartType(type: Int): Int {
         return when(type) {
             2 -> {
-                (1000 * 60 * 60 * 24) * 7
+                -7
             }
             3 -> {
-                (1000 * 60 * 60 * 24) * 3
+                -3
             }
             4 -> {
-                (1000 * 60 * 60 * 24) * 1
+                -1
             }
             else -> {
                 0
