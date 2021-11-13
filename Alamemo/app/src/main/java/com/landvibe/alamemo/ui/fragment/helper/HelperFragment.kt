10ca97381a8d2a2e51eb21105c304ff2680ba053
KHS-PC\pskna <psknal27@gmail.com>
2021-11-13 20:28:33 +0900

    feat. 개발자에게 문의하기 기능 추가
    -Intent에 type을 설정한 후 data에 email을 넣어주는 방식.

--- Alamemo/app/src/main/java/com/landvibe/alamemo/ui/fragment/helper/HelperFragment.kt
+++ Alamemo/app/src/main/java/com/landvibe/alamemo/ui/fragment/helper/HelperFragment.kt
@@ -1,5 +1,9 @@
 package com.landvibe.alamemo.ui.fragment.helper
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.landvibe.alamemo.R
 import com.landvibe.alamemo.adapter.HelperRecyclerViewAdapter
@@ -17,7 +21,7 @@ class HelperFragment: BaseFragment<FragmentHelperBinding>() {
 
     private fun setUpBtnClickListener() {
         viewDataBinding.backButton.setOnClickListener { requireActivity().onBackPressed() }
-        viewDataBinding.mailToDeveloperBtn.setOnClickListener {  }
+        viewDataBinding.mailToDeveloperBtn.setOnClickListener { sendEmailToDeveloper() }
     }
 
     private fun setUpFunctionRecyclerView() {
@@ -31,5 +35,21 @@ class HelperFragment: BaseFragment<FragmentHelperBinding>() {
         )
         viewDataBinding.functionRecyclerView.adapter = HelperRecyclerViewAdapter(requireContext(), helperList)
         viewDataBinding.functionRecyclerView.layoutManager = LinearLayoutManager(context)
+    }
+
+    //개발자에게 이메일을 보내는 메서드. https://deumdroid.tistory.com/ 참고
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun sendEmailToDeveloper() {
+        val emailAddress = "psknal27@gmail.com"
+
+        val intent = Intent(Intent.ACTION_SEND)
+            .apply {
+                type = "plain/text"
+
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(emailAddress))
+                putExtra(Intent.EXTRA_SUBJECT, "")
+                putExtra(Intent.EXTRA_TEXT, "")
+            }
+        startActivity(intent)
     }
 }
