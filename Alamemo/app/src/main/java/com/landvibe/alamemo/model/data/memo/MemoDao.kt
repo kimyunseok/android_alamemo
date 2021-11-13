9f9bcb97a3f4a89be441f3c41b3eab93ed7bf51c
KHS-PC\pskna <psknal27@gmail.com>
2021-11-13 13:57:42 +0900

    fix. DAO 수정
    - API 26 이하 버전에서 인식못하는 문제가 발생.
    - 날짜 수정하는 메서드 추가

--- Alamemo/app/src/main/java/com/landvibe/alamemo/model/data/memo/MemoDao.kt
+++ Alamemo/app/src/main/java/com/landvibe/alamemo/model/data/memo/MemoDao.kt
@@ -20,20 +20,20 @@ interface MemoDao {
     @Query("SELECT * FROM Memo Where id = :id")
     fun getMemoById(id: Long): Memo
 
-    @Query("SELECT * FROM Memo Where type = :type and scheduleFinish = ${false}")
-    fun getMemoByType(type: Int): List<Memo>
+    @Query("SELECT * FROM Memo Where type = :type and scheduleFinish = :scheduleFinish")
+    fun getMemoByType(type: Int, scheduleFinish: Boolean = false): List<Memo>
 
-    @Query("SELECT * FROM Memo Where scheduleFinish = ${true}")
-    fun getFinishMemo(): List<Memo>
+    @Query("SELECT * FROM Memo Where scheduleFinish = :scheduleFinish")
+    fun getFinishMemo(scheduleFinish: Boolean = true): List<Memo>
 
-    @Query("SELECT * FROM Memo Where setAlarm = ${true}")
-    fun getAlarmMemo(): List<Memo>
+    @Query("SELECT * FROM Memo Where setAlarm = :setAlarm")
+    fun getAlarmMemo(setAlarm: Boolean = true): List<Memo>
 
-    @Query("SELECT * FROM Memo Where fixNotify = ${true}")
-    fun getFixNotifyMemo(): List<Memo>
+    @Query("SELECT * FROM Memo Where fixNotify = :fixNotify")
+    fun getFixNotifyMemo(fixNotify: Boolean = true): List<Memo>
 
-    @Query("SELECT * FROM Memo Where type = ${3}")
-    fun getRepeatScheduleMemo(): List<Memo>
+    @Query("SELECT * FROM Memo Where type = :type")
+    fun getRepeatScheduleMemo(type: Int = 3): List<Memo>
 
     @Query("DELETE FROM Memo Where id = :id")
     fun deleteMemoByID(id: Long)
@@ -41,8 +41,8 @@ interface MemoDao {
     @Query("UPDATE Memo SET type = :type Where id = :id")
     fun modifyMemoType(id: Long, type: MutableLiveData<Int>)
 
-    @Query("UPDATE Memo SET scheduleFinish = ${true} Where id = :id")
-    fun setMemoFinish(id: Long)
+    @Query("UPDATE Memo SET scheduleFinish = :scheduleFinish Where id = :id")
+    fun setMemoFinish(id: Long, scheduleFinish: Boolean = true)
 
     @Query("UPDATE Memo SET type = :type, icon = :icon, title = :title, scheduleDateYear = :scheduleDateYear, scheduleDateMonth = :scheduleDateMonth, scheduleDateDay = :scheduleDateDay, scheduleDateHour = :scheduleDateHour, scheduleDateMinute = :scheduleDateMinute, alarmStartTimeHour = :alarmStartTimeHour, alarmStartTimeMinute = :alarmStartTimeMinute, scheduleFinish = :scheduleFinish, fixNotify = :fixNotify, setAlarm = :setAlarm, repeatDay = :repeatDay, alarmStartTimeType = :alarmStartTimeType Where id = :id")
     fun modifyMemo(id: Long, type: MutableLiveData<Int>, icon: MutableLiveData<String>,
@@ -51,5 +51,9 @@ interface MemoDao {
                    scheduleDateHour: MutableLiveData<Int>, scheduleDateMinute: MutableLiveData<Int>,
                    alarmStartTimeHour: MutableLiveData<Int>, alarmStartTimeMinute: MutableLiveData<Int>, scheduleFinish: MutableLiveData<Boolean>,
                    fixNotify: MutableLiveData<Boolean>, setAlarm: MutableLiveData<Boolean>, repeatDay: MutableList<Char>, alarmStartTimeType: MutableLiveData<Int>)
+
+    @Query("UPDATE Memo SET scheduleDateYear = :scheduleDateYear, scheduleDateMonth = :scheduleDateMonth, scheduleDateDay = :scheduleDateDay Where id = :id")
+    fun modifyMemoDate(id: Long, scheduleDateYear: MutableLiveData<Int>,
+                   scheduleDateMonth: MutableLiveData<Int>, scheduleDateDay: MutableLiveData<Int>)
 
 }
